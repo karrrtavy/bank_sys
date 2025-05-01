d76aec949ecf6702962331e03af636ffd89b24d6
kushevada <dasha.kusheva18@mail.ru>
2025-05-01 20:26:37 +0300

    внесены некоторые изменения в форме регистрации и аунтетификации

--- bank_sys/module_auth/forms.py
+++ bank_sys/module_auth/forms.py
@@ -3,14 +3,43 @@ from django.contrib.auth.forms import AuthenticationForm
 from .models import User
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    repeat_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput,
+        label="Пароль",
+        help_text="Введите надежный пароль"
+    )
+    repeat_password = forms.CharField(
+        widget=forms.PasswordInput,
+        label="Повторите пароль",
+        help_text="Повторите пароль для подтверждения"
+    )
     
     class Meta:
         model = User
         fields = ['name', 'surname', 'patronymic', 
-                  'phone', 'income', 'password', 
-                  'repeat_password']
+                'phone', 'income', 'password', 
+                'repeat_password']
+        labels = {
+            'name': 'Имя',
+            'surname': 'Фамилия',
+            'patronymic': 'Отчество',
+            'phone': 'Телефон',
+            'income': 'Доход',
+        }
+        help_texts = {
+            'phone': 'Формат: +71234567890',
+            'income': 'Укажите ваш ежемесячный доход',
+            'name': 'Только русские буквы',
+            'surname': 'Только русские буквы',
+            'patronymic': 'Только русские буквы',
+        }
 
 class CustomLoginForm(AuthenticationForm):
-    username = forms.CharField(label="Логин или телефон")
+    username = forms.CharField(
+        label="Логин или телефон",
+        help_text="Введите телефон (+7...) или имя пользователя"
+    )
+    password = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput
+    )
